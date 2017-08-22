46b11069db9fcaaab797e28dad54030d8193b105
Manvir <mail@manvir.me>
2017-08-22 18:05:38 +0530

    Database schema creation

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Stock(models.Model):
+    symbol = models.CharField(max_length=20, primary_key=True)
+    added_on = models.DateField(auto_now_add=True)
+    modified_on = models.DateField(auto_now=True)
+
+
+class Price(models.Model):
+    stock = models.ForeignKey(Stock)
+    date = models.DateField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
