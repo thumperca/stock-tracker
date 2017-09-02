503b19936bad3d17244b7be4318df7386e34e829
Manvir <mail@manvir.me>
2017-09-02 10:30:41 +0530

    Added price field to stock model

--- tracker/models.py
+++ tracker/models.py
@@ -4,6 +4,7 @@ from tracker.mixins import StockMixin
 
 class Stock(models.Model, StockMixin):
     symbol = models.CharField(max_length=20, primary_key=True)
+    last_price = models.DecimalField(max_digits=9, decimal_places=2, null=True)
     added_on = models.DateField(auto_now_add=True)
     modified_on = models.DateField(auto_now=True)
 
