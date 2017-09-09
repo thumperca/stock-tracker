6fb6ec07280664db40132aa99b48c8fdb904897c
Manvir <mail@manvir.me>
2017-09-09 16:36:07 +0530

    Added stock search on homepage

--- react/components/pages/Home.js
+++ react/components/pages/Home.js
@@ -13,8 +13,11 @@ export default class Home extends React.Component {
             //  loading status of page
             //  true when loading data from server
             loading: false,
+            //  list of stocks loaded from server
+            all : [],
             //  list of stocks to show on screen
             stocks: []
+
         }
     }
 
@@ -22,13 +25,31 @@ export default class Home extends React.Component {
     componentDidMount(props) {
         if ( !props ) props = this.props;
         this.setState({loading: true})
-        axios.get('/api/stocks?q='+props.page).then(res => this.setState({loading: false, stocks: res.data}))
+        axios.get('/api/stocks?q='+props.page).then(res => {
+            this.setState({loading: false, stocks: res.data, all: res.data})
+        })
     }
 
     //  update stock when user switches b/w portfolio & watchlist
     componentWillReceiveProps(nextProps) {
         if ( nextProps.view == this.props.view ) return;
         this.componentDidMount(nextProps);
+    }
+
+    search(event) {
+        let value = event.target.value.replace(' ', '').toUpperCase();
+        if ( !value.length ) {
+            this.setState({stocks: this.state.all});
+            return;
+        }
+        const stocks = this.state.all;
+        let list = [];
+        for ( let i in stocks ) {
+            let symbol = stocks[i].symbol;
+            if ( symbol.indexOf(value) == -1 ) continue;
+            list.push(stocks[i]);
+        }
+        this.setState({stocks: list});
     }
 
     render() {
@@ -39,6 +60,7 @@ export default class Home extends React.Component {
         return (
             <div class="container">
                 <h2>Stocks in Shortlist</h2>
+                <input id="quick-search" onChange={e => this.search(e)} type="search" class="form-control" placeholder="Search..." />
                 <List stocks={stocks} />
             </div>
         )
